7be3d4547d091d298b2afeb7cd1468df30221766
vercluka <lukanaguglu@gmail.com>
2025-01-06 16:40:41 +0100

    CevFix

--- Todo/todo-backend/src/test/java/com/example/todo_backend/ToDoBackendApplicationTests.java
+++ Todo/todo-backend/src/test/java/com/example/todo_backend/ToDoBackendApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToDoBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
