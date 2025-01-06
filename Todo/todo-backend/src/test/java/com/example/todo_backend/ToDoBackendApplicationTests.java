ba5659f6cfefb56e9e01ffdd8b53638703e92659
vercluka <lukanaguglu@gmail.com>
2025-01-06 16:45:21 +0100

    Testus Maximus 2

--- Todo/todo-backend/src/test/java/com/example/todo_backend/ToDoBackendApplicationTests.java
+++ Todo/todo-backend/src/test/java/com/example/todo_backend/ToDoBackendApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.todo_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class ToDoBackendApplicationTests {
